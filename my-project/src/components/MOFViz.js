8f09249f24b0092c1856b8ef24186c489b2b5fe8
senmeetechin <techin1021@gmaiil.com>
2023-04-02 22:08:06 +0700

    ✨ MOF visualization from CIF

--- my-project/src/components/MOFViz.js
+++ my-project/src/components/MOFViz.js
@@ -1,17 +1,21 @@
 import * as $3Dmol from "3dmol/build/3Dmol.js";
 import React, { Component } from 'react';
+import mof_file from '../assets/str_m1_o1_o1_pcu_sym.9.cif'
 
 class MOFViz extends Component {
   componentDidMount() {
     // Initialize the 3Dmol.js viewer
     var container = document.getElementById("viewer");
     var viewer = $3Dmol.createViewer(container);
+    var file = mof_file
 
     // Load the CIF file
-    $3Dmol.download("pdb:1MO8",viewer,{multimodel:true, frames:true},function(){
-        viewer.setStyle({}, {cartoon:{color:"spectrum"}});
-        viewer.render();
-    });
+    $3Dmol.get(file, function(data) {
+        viewer.addModel(data,'cif');
+        viewer.setStyle({sphere:{}});
+        viewer.zoomTo();
+        viewer.render( );
+      });
   }
 
   render() {
